fa59a8969848c49af798a4ea3ad047e55f209f4a
NGauville <noreo.ercoli@gmail.com>
2017-04-30 12:21:23 +0200

    counterWT

--- src/main/CounterWT.java
+++ src/main/CounterWT.java
@@ -5,16 +5,28 @@ import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * Water Touched Counter panel
+ */
 public class CounterWT extends JPanel {
 	{
         setOpaque(false);
     }
-	public CounterWT(int a, int b) {
+	
+	private JLabel water, touched;
+	
+	public CounterWT(int waterValue, int touchedValue) {
 		super();
 		setLayout(new BorderLayout());
-		JLabel lab1 = new JLabel("Eau : "+a,JLabel.CENTER);
-		add(lab1,BorderLayout.NORTH);
-		JLabel lab2 = new JLabel("Touché : "+b,JLabel.CENTER);
-		add(lab2,BorderLayout.CENTER);
+		
+		add(water = new JLabel(" ", JLabel.CENTER), BorderLayout.NORTH);
+		add(touched = new JLabel(" ", JLabel.CENTER), BorderLayout.CENTER);
+		
+		updateValue(waterValue, touchedValue);
+	}
+	
+	public void updateValue(int waterValue, int touchedValue) {
+		water.setText("Eau : " + waterValue);
+		touched.setText("Touché : " + touchedValue);
 	}
 }
