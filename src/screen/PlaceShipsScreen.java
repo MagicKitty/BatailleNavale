113806e69463751720fdae5cd5b9ac801f69901e
NGauville <noreo.ercoli@gmail.com>
2017-04-30 11:44:07 +0200

    style bateaux placement

--- src/screen/PlaceShipsScreen.java
+++ src/screen/PlaceShipsScreen.java
@@ -70,7 +70,9 @@ public class PlaceShipsScreen extends GridScreen {
 		
 		if(carrier.getX() != -1) {
 			if(carrier.getX() < 6) {
-				for(int i = 0; i < 5; i++) colorCell(carrier.getX() + i, carrier.getY(), 2);
+				colorCell(carrier.getX(), carrier.getY(), 1);
+				for(int i = 1; i < 4; i++) colorCell(carrier.getX() + i, carrier.getY(), 5);
+				colorCell(carrier.getX() + 4, carrier.getY(), 3);
 			} else {
 				for(int i = 0; i < 5; i++) if(carrier.getX() + i < 10) colorCell(carrier.getX() + i, carrier.getY(),-1);
 			}
@@ -78,7 +80,9 @@ public class PlaceShipsScreen extends GridScreen {
 		
 		if(cruiser.getX() != -1) {
 			if(cruiser.getX() < 7) {
-				for(int i = 0; i < 4; i++) colorCell(cruiser.getX() + i, cruiser.getY(), 2);
+				colorCell(cruiser.getX(), cruiser.getY(), 1);
+				for(int i = 1; i < 3; i++) colorCell(cruiser.getX() + i, cruiser.getY(), 5);
+				colorCell(cruiser.getX() + 3, cruiser.getY(), 3);
 			} else {
 				for(int i = 0; i < 4; i++) if(cruiser.getX() + i < 10) colorCell(cruiser.getX() + i, cruiser.getY(),-1);
 			}
@@ -86,7 +90,8 @@ public class PlaceShipsScreen extends GridScreen {
 		
 		if(battleship.getX() != -1) {
 			if(battleship.getX() < 9) {
-				for(int i = 0; i < 2; i++) colorCell(battleship.getX() + i, battleship.getY(), 2);
+				colorCell(battleship.getX(), battleship.getY(), 1);
+				colorCell(battleship.getX() + 1, battleship.getY(), 3);
 			} else {
 				for(int i = 0; i < 2; i++) if(battleship.getX() + i < 10) colorCell(battleship.getX() + i, battleship.getY(),-1);
 			}
@@ -94,7 +99,9 @@ public class PlaceShipsScreen extends GridScreen {
 		
 		if(submarine.getX() != -1) {
 			if(submarine.getX() < 9) {
-				for(int i = 0; i < 3; i++) colorCell(submarine.getX() + i, submarine.getY(), 2);
+				colorCell(submarine.getX(), submarine.getY(), 1);
+				colorCell(submarine.getX() + 1, submarine.getY(), 5);
+				colorCell(submarine.getX() + 2, submarine.getY(), 3);
 			} else {
 				for(int i = 0; i < 3; i++) if(submarine.getX() + i < 10) colorCell(submarine.getX() + i, submarine.getY(), -1);
 			}
@@ -102,7 +109,9 @@ public class PlaceShipsScreen extends GridScreen {
 		
 		if(destroyer.getX() != -1) {
 			if(destroyer.getX() < 9) {
-				for(int i = 0; i < 3; i++) colorCell(destroyer.getX() + i, destroyer.getY(), 2);
+				colorCell(destroyer.getX(), destroyer.getY(), 1);
+				colorCell(destroyer.getX() + 1, destroyer.getY(), 5);
+				colorCell(destroyer.getX() + 2, destroyer.getY(), 3);
 			} else {
 				for(int i = 0; i < 3; i++) if(destroyer.getX() + i < 10) colorCell(destroyer.getX() + i, destroyer.getY(), -1);
 			}
@@ -112,10 +121,33 @@ public class PlaceShipsScreen extends GridScreen {
 	private void updateScreen() {
 		clearScreen();
 		
-		if(carrierPos.getX() != -1) for(int i = 0; i < 5; i++) colorCell(carrierPos.getX() + i, carrierPos.getY(), 1);
-		if(cruiserPos.getX() != -1) for(int i = 0; i < 4; i++) colorCell(cruiserPos.getX() + i, cruiserPos.getY(), 1);
-		if(battleshipPos.getX() != -1) for(int i = 0; i < 2; i++) colorCell(battleshipPos.getX() + i, battleshipPos.getY(), 1);
-		if(submarinePos.getX() != -1) for(int i = 0; i < 3; i++) colorCell(submarinePos.getX() + i, submarinePos.getY(), 1);
-		if(destroyerPos.getX() != -1) for(int i = 0; i < 3; i++) colorCell(destroyerPos.getX() + i, destroyerPos.getY(), 1);
+		if(carrierPos.getX() != -1) {
+			colorCell(carrierPos.getX(), carrierPos.getY(), 1);
+			for(int i = 1; i < 4; i++) colorCell(carrierPos.getX() + i, carrierPos.getY(), 5);
+			colorCell(carrierPos.getX() + 4, carrierPos.getY(), 3);
+		}
+		
+		if(cruiserPos.getX() != -1) {
+			colorCell(cruiserPos.getX(), cruiserPos.getY(), 1);
+			for(int i = 1; i < 3; i++) colorCell(cruiserPos.getX() + i, cruiserPos.getY(), 5);
+			colorCell(cruiserPos.getX() + 3, cruiserPos.getY(), 3);
+		}
+		
+		if(battleshipPos.getX() != -1) {
+			colorCell(battleshipPos.getX(), battleshipPos.getY(), 1);
+			colorCell(battleshipPos.getX() + 1, battleshipPos.getY(), 3);
+		}
+		
+		if(submarinePos.getX() != -1) {
+			colorCell(submarinePos.getX(), submarinePos.getY(), 1);
+			colorCell(submarinePos.getX() + 1, submarinePos.getY(), 5);
+			colorCell(submarinePos.getX() + 2, submarinePos.getY(), 3);
+		}
+		
+		if(destroyerPos.getX() != -1) {
+			colorCell(destroyerPos.getX(), destroyerPos.getY(), 1);
+			colorCell(destroyerPos.getX() + 1, destroyerPos.getY(), 5);
+			colorCell(destroyerPos.getX() + 2, destroyerPos.getY(), 3);
+		}
 	}
 }
