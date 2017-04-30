6de3bc211c0ed524a182871dac35ad008262a609
NGauville <noreo.ercoli@gmail.com>
2017-04-30 11:56:41 +0200

    controlleur placement bateaux

--- src/controller/PlaceShipsController.java
+++ src/controller/PlaceShipsController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+import main.GameStatus;
+import main.GameWindow;
 import ship.ShipType;
 
-public class PlaceShipsController {
+public class PlaceShipsController implements ButtonController {
+	private GameWindow gamewindow;
 	private ShipType addedType;
+	private boolean carrierReady, cruiserReady, battleshipReady, destroyerReady, submarineReady;
 	
-	public PlaceShipsController() {
+	public PlaceShipsController(GameWindow gw) {
+		gamewindow = gw;
+		carrierReady = cruiserReady = battleshipReady = destroyerReady = submarineReady = false;
 		addedType = ShipType.CARRIER;
 	}
 	
@@ -31,5 +40,42 @@ public class PlaceShipsController {
 	
 	public void chooseSubmarine() {
 		addedType = ShipType.SUBMARINE;
+	}
+	
+	public boolean isReady() {
+		return carrierReady && cruiserReady && battleshipReady && destroyerReady && submarineReady;
+	}
+	
+	public void setShipStatus(ShipType ship, boolean status) {
+		switch(ship) {
+			case CARRIER: carrierReady = status; break;
+			case CRUISER: cruiserReady = status; break;
+			case BATTLESHIP: battleshipReady = status; break;
+			case DESTROYER: destroyerReady = status; break;
+			case SUBMARINE: submarineReady = status; break;
+		}
+	}
+	
+	public boolean getShipStatus(ShipType ship) {
+		boolean status = false;
+		switch(ship) {
+			case CARRIER: status = carrierReady; break;
+			case CRUISER: status = cruiserReady; break;
+			case BATTLESHIP: status = battleshipReady; break;
+			case DESTROYER: status = destroyerReady; break;
+			case SUBMARINE: status = submarineReady; break;
+		}
+		return status;
+	}
+	
+	@Override
+	public void handleButton(Object s) {
+		if(s instanceof JButton && ((JButton) s).getText() == "Continue") {
+			if(isReady()) {
+				gamewindow.setStatus(GameStatus.INGAME);
+			} else {
+				(new JOptionPane()).showMessageDialog(null, "Vous devez placer vos cinq bateaux !", "Erreur", JOptionPane.ERROR_MESSAGE);
+			}
+		}
 	}
 }
--- src/screen/PlaceShipsScreen.java
+++ src/screen/PlaceShipsScreen.java
@@ -2,6 +2,7 @@ package screen;
 
 import controller.PlaceShipsController;
 import graphic.Coord2D;
+import ship.ShipType;
 
 public class PlaceShipsScreen extends GridScreen {
 	private PlaceShipsController controller;
@@ -122,29 +123,39 @@ public class PlaceShipsScreen extends GridScreen {
 		clearScreen();
 		
 		if(carrierPos.getX() != -1) {
+			controller.setShipStatus(ShipType.CARRIER, true);
+			
 			colorCell(carrierPos.getX(), carrierPos.getY(), 1);
 			for(int i = 1; i < 4; i++) colorCell(carrierPos.getX() + i, carrierPos.getY(), 5);
 			colorCell(carrierPos.getX() + 4, carrierPos.getY(), 3);
 		}
 		
 		if(cruiserPos.getX() != -1) {
+			controller.setShipStatus(ShipType.CRUISER, true);
+			
 			colorCell(cruiserPos.getX(), cruiserPos.getY(), 1);
 			for(int i = 1; i < 3; i++) colorCell(cruiserPos.getX() + i, cruiserPos.getY(), 5);
 			colorCell(cruiserPos.getX() + 3, cruiserPos.getY(), 3);
 		}
 		
 		if(battleshipPos.getX() != -1) {
+			controller.setShipStatus(ShipType.BATTLESHIP, true);
+			
 			colorCell(battleshipPos.getX(), battleshipPos.getY(), 1);
 			colorCell(battleshipPos.getX() + 1, battleshipPos.getY(), 3);
 		}
 		
 		if(submarinePos.getX() != -1) {
+			controller.setShipStatus(ShipType.SUBMARINE, true);
+			
 			colorCell(submarinePos.getX(), submarinePos.getY(), 1);
 			colorCell(submarinePos.getX() + 1, submarinePos.getY(), 5);
 			colorCell(submarinePos.getX() + 2, submarinePos.getY(), 3);
 		}
 		
 		if(destroyerPos.getX() != -1) {
+			controller.setShipStatus(ShipType.DESTROYER, true);
+			
 			colorCell(destroyerPos.getX(), destroyerPos.getY(), 1);
 			colorCell(destroyerPos.getX() + 1, destroyerPos.getY(), 5);
 			colorCell(destroyerPos.getX() + 2, destroyerPos.getY(), 3);
